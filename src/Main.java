5f3455d0fcbfb6c2844d4f175e8525dd32167025
Vel <korzhavin.vel@yandex.ru>
2023-07-04 16:02:15 +0700

    HomeWork6 task1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-
-
+    task1();
     }
+
+        public static void task1 () {
+            System.out.println("Задача 1");
+        for (int i = 0; i < 10; i++)
+            System.out.println("Итерация цикла " + i);
+        }
+
+
+
+
 }
