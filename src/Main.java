be3a9f86ec14a2726938121947e50846b881aae4
Vel <korzhavin.vel@yandex.ru>
2023-07-04 16:23:04 +0700

    HomeWork6 task6

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1() {
@@ -41,6 +42,13 @@ public class Main {
         System.out.println("Задача 5");
         for (int i = 1904; i < 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 0; i < 100; i = i + 7) {
+            System.out.println("Итерация цикла " + i);
         }
     }
 
