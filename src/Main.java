e392de08f16bcc0b7f572f64d14b9c6f799f7fa2
Vel <korzhavin.vel@yandex.ru>
2023-07-04 16:24:47 +0700

    HomeWork6 task7

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         task4();
         task5();
         task6();
+        task7();
     }
 
     public static void task1() {
@@ -48,6 +49,13 @@ public class Main {
     public static void task6() {
         System.out.println("Задача 6");
         for (int i = 0; i < 100; i = i + 7) {
+            System.out.println("Итерация цикла " + i);
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println("Итерация цикла " + i);
         }
     }
