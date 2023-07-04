8327f85e43e043c3cbdd529ef72bd06c84255647
Vel <korzhavin.vel@yandex.ru>
2023-07-04 16:36:38 +0700

    HomeWork6 task8

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
     }
 
     public static void task1() {
@@ -58,6 +59,17 @@ public class Main {
         for (int i = 1; i <= 512; i = i * 2) {
             System.out.println("Итерация цикла " + i);
         }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
+        }
+        System.out.println(total);
     }
 
 
